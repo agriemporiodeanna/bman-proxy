4857d4525367a6fa143f8fb1a96abec326c86c5d
agriemporiodeanna <shop@agriemporiodeanna.com>
2025-12-04 01:00:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,42 @@ import fetch from "node-fetch";
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Creazione SOAP Envelope
+function buildSOAPEnvelope(method, params) {
+  return `
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+               xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <${method} xmlns="http://tempuri.org/">
+      <chiave>${params.chiave}</chiave>
+      <filtri>${params.filtri}</filtri>
+      <ordinamentoCampo>${params.ordinamentoCampo}</ordinamentoCampo>
+      <ordinamentoDirezione>${params.ordinamentoDirezione}</ordinamentoDirezione>
+      <numeroPagina>${params.numeroPagina}</numeroPagina>
+      <listaDepositi>${params.listaDepositi}</listaDepositi>
+      <dettaglioVarianti>${params.dettaglioVarianti}</dettaglioVarianti>
+    </${method}>
+  </soap:Body>
+</soap:Envelope>
+  `.trim();
+}
+
 app.post("/bman", async (req, res) => {
 
+  const method = "getAnagrafiche"; // Metodo SOAP
   const bmanUrl = "https://emporiodeanna.bman.it:3555/bmanapi.asmx";
 
-  // Aggiungiamo il nome del metodo richiesto
-  const params = new URLSearchParams({
-    nomeMetodo: "getAnagrafiche",
-    ...req.body
-  });
+  const soapEnvelope = buildSOAPEnvelope(method, req.body);
 
   try {
     const response = await fetch(bmanUrl, {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: params.toString()
+      headers: {
+        "Content-Type": "text/xml; charset=utf-8",
+        "SOAPAction": `http://tempuri.org/${method}`
+      },
+      body: soapEnvelope
     });
 
     const xml = await response.text();
@@ -33,14 +54,11 @@ app.post("/bman", async (req, res) => {
     return res.json(JSON.parse(match[1]));
 
   } catch (err) {
-    return res.status(500).send("Errore proxy: " + err.toString());
+    return res.status(500).send("Errore proxy SOAP: " + err.toString());
   }
 });
 
+// Avvio server
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Bman proxy attivo con metodo corretto!");
+  console.log("Bman SOAP proxy attivo!");
 });
-
-
-
-
